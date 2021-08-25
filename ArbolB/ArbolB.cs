0341c14c8ed2640d57785fc0795e487a006cf46c
DianaAragon <78452952+DianaAragon@users.noreply.github.com>
2021-08-25 15:21:17 -0600

    Update ArbolB.cs

--- ArbolB/ArbolB.cs
+++ ArbolB/ArbolB.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +6,8 @@ namespace ArbolB
 {
     public class ArbolB<T>
     {
+        public bool ValorRepitodo;
+        public bool valoringresado;
         internal Node<T> root;
         internal int n;
         public delegate int Comparador<T>(T a, T b);
@@ -15,10 +17,24 @@ namespace ArbolB
             this.n = n;
             this.comparador = Funcomparador;// el apuntador de la linea 12 que apunte a la funcion. 
         }
+     
+        public bool IsEmpty()
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            else if (root.IsEmpy()) 
+            {
+                return true;
+            }
+            return false;
+        }
+        
 
         public void Add(T dato)
         {
-
+            valoringresado = false;
             if (root == null)
             {
                 root = new Node<T>(n, comparador);
@@ -26,77 +42,127 @@ namespace ArbolB
             }
             else
             {
-                add2(dato, root, null);
+                add2(dato, root,null);
             }
-
+           
         }
+        
+        
         private void add2(T dato, Node<T> Actual, Node<T> AnteriorActual)
         {
-            if (find(dato, Actual) == false)
+            bool ciclo = false;
+            find(dato, Actual);
+            if (ValorRepitodo!=true)
             {
-                if (Actual.IsFull())
+                if (Actual.tienehijos())
                 {
-                    if (Actual.tienehijos())
+                    int i = 0;
+                    while (Actual.nodosInternos[i] != null && ciclo != true)
                     {
-                        int i = 0;
-                        while (Actual.nodosInternos[i].Value != null)
+                        if (comparador.Invoke(dato, Actual.nodosInternos[i].Value) == -1)
                         {
-                            if (comparador.Invoke(dato, Actual.nodosInternos[i].Value) == -1)
-                            {
-                                add2(dato, Actual.nodosInternos[i].Left, Actual);
-                            }
-
-                            if (Actual.nodosInternos[i + 1] == null)
-                            {
-                                add2(dato, Actual.nodosInternos[i].Right, Actual);
-                            }
-                            i++;
+                            add2(dato, Actual.nodosInternos[i].Left, Actual);
+                            ciclo = true;
+                        }
+                        else if (Actual.nodosInternos[i + 1] == null)
+                        {
+                            add2(dato, Actual.nodosInternos[i].Right, Actual);
+                            ciclo = true;
+                        }
+                        i++;
+                    }
+                }
+                if (valoringresado == false)
+                {
+                    if (Actual.IsFull2())
+                    {
+                        Node<T> Left = new Node<T>(n, comparador);
+                        Node<T> Right = new Node<T>(n, comparador);
+                        valoringresado = true;
+                        Node<T> aux = new Node<T>(n, comparador);
+                        int k = 0;
+                        foreach (var item in Actual.nodosInternos)
+                        {
+                            aux.nodosInternos[k] = item;
+                            k++;
+                        }
+                        aux.AddValue(dato);
+                        if (AnteriorActual == null)
+                        {
+                            partirraiz(ref Left, ref Right, ref Actual, ref aux);
+                            int x = 0;
+                        }
+                        else
+                        {
+                            subir(ref Right, ref Actual, ref aux, ref AnteriorActual);
                         }
                     }
                     else
                     {
-                        if (AnteriorActual == null)
-                        {
-                            Node<T> aux = new Node<T>(n + 1, comparador);
-                            int k = 0;
-                            foreach (var item in Actual.nodosInternos)
-                            {
-                                aux.nodosInternos[k] = item;
-                                k++;
-                            }
-                            aux.AddValue(dato);
-                            Node<T> Left = new Node<T>(n, comparador);
-                            Node<T> Right = new Node<T>(n, comparador);
-                            Node<T> Tempactual = new Node<T>(n, comparador);
-                            Tempactual = Actual;
-                            Actual = new Node<T>(n, comparador);
-                            Actual.AddValue(Divide(aux, ref Left, ref Right));
-                            Actual.nodosInternos[0].Left = Left;
-                            Actual.nodosInternos[0].Right = Right;
-                        }
-
+                        Actual.AddValue(dato);
+                        valoringresado = true;
                     }
                 }
                 else
                 {
-                    Actual.AddValue(dato);
-                    if (Actual.tienehijos())
+                    if (Actual.IsFull())
                     {
-
+                        Node<T> Left = new Node<T>(n, comparador);
+                        Node<T> Right = new Node<T>(n, comparador);
+                        Node<T> Temp = new Node<T>(n, comparador);
+                        int k = 0;
+                        foreach (var item in Actual.nodosInternos)
+                        {
+                            Temp.nodosInternos[k] = item;
+                            k++;
+                        }
+                        if (AnteriorActual == null)
+                        {
+                            partirraiz(ref Left, ref Right, ref Actual, ref Temp);
+                        }
+                        else
+                        {
+                            subir(ref Right, ref Actual, ref Temp, ref AnteriorActual);
+                        }
                     }
                 }
             }
+           
+            
+        }
+        internal void partirraiz(ref Node<T> Left, ref Node<T> Right, ref Node<T> Actual, ref Node<T> Temp)
+        {
+            Actual.empty();
+            Actual.AddValue(Divide(Temp, ref Left, ref Right));
+            Actual.nodosInternos[0].Left = Left;
+            Actual.nodosInternos[0].Right = Right;
+        }
+        internal void subir(ref Node<T> Right, ref Node<T> Actual, ref Node<T> Temp, ref Node<T> AnteriorActual)
+        {
 
+            Actual.empty();
+            Divide(Temp, ref Actual, ref Right);
+            AnteriorActual.AddValue(ValorEnMedio(Temp));
+            AnteriorActual.nodosInternos[findpos(ValorEnMedio(Temp), AnteriorActual)].Left = Actual;
+            AnteriorActual.nodosInternos[findpos(ValorEnMedio(Temp), AnteriorActual)].Right = Right;
+            if (!(AnteriorActual.IsFull() && findpos(ValorEnMedio(Temp), AnteriorActual)==n-1))
+            {
+                if (AnteriorActual.nodosInternos[findpos(ValorEnMedio(Temp), AnteriorActual) + 1] != null)
+                {
+                    AnteriorActual.nodosInternos[findpos(ValorEnMedio(Temp), AnteriorActual) + 1].Left = AnteriorActual.nodosInternos[findpos(ValorEnMedio(Temp), AnteriorActual)].Right;
+                }
+            }
+           
         }
         internal T Divide(Node<T> arr, ref Node<T> left, ref Node<T> right)
         {
             for (int i = 0; i < arr.nodosInternos.Length / 2; i++)
             {
-                left.AddValue(arr.nodosInternos[i].Value);
+                left.AddValue2(arr.nodosInternos[i]);
             }
             for (int i = arr.nodosInternos.Length / 2 + 1; i < arr.nodosInternos.Length; i++)
             {
-                right.AddValue(arr.nodosInternos[i].Value);
+                right.AddValue2(arr.nodosInternos[i]);
             }
             return arr.nodosInternos[arr.nodosInternos.Length / 2].Value;
         }
@@ -104,45 +170,61 @@ namespace ArbolB
         {
             return arr.nodosInternos[arr.nodosInternos.Length / 2].Value;
         }
-        private bool find(T dato, Node<T> Actual)
+        private int findpos(T dato, Node<T> Actual)
         {
+            for (int x = 0; x < n; x++)
+            {
+                if (comparador.Invoke(dato, Actual.nodosInternos[x].Value) == 0)
+                {
+                    return x;
+                }
+            }
+            return -1;
+        }
+        private void find(T dato,Node<T> Actual)
+        {
+            ValorRepitodo = false;
+            bool ciclo = false;
             if (Actual.tienehijos())
             {
+                
                 int i = 0;
-                while (Actual.nodosInternos[i].Value != null)
+                while (Actual.nodosInternos[i] != null && ciclo!=true)
                 {
                     if (comparador.Invoke(dato, Actual.nodosInternos[i].Value) == 0)
                     {
-                        return true;
+                        ValorRepitodo = true;
                     }
                     else if (comparador.Invoke(dato, Actual.nodosInternos[i].Value) == -1)
                     {
                         find(dato, Actual.nodosInternos[i].Left);
+                        ciclo = true;
                     }
                     else if (Actual.nodosInternos[i + 1] == null)
                     {
                         find(dato, Actual.nodosInternos[i].Right);
+                        ciclo = true;
                     }
                     i++;
                 }
-                return false;
+                
             }
             else
             {
-                for (int x = 0; x < n - 1; x++)
+                for (int x=0;x< n-1;x++)
                 {
                     if (Actual.nodosInternos[x] != null)
                     {
                         if (comparador.Invoke(dato, Actual.nodosInternos[x].Value) == 0)
                         {
-                            return true;
+                            ValorRepitodo = true;
                         }
                     }
-
+                    
 
                 }
-                return false;
             }
+            
         }
 
     }
