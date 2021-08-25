6231c6cd476e2f434ba080105ba31402c41dbce6
DianaAragon <78452952+DianaAragon@users.noreply.github.com>
2021-08-24 21:07:36 -0600

    llenar raíz y dividirla

--- ArbolB/Node.cs
+++ ArbolB/Node.cs
@@ -6,26 +6,25 @@ namespace ArbolB
 {
     class Node<T>
     {
-        internal NodosInternos<T>[] nodosInternos;       
-        private int n;
+        internal NodosInternos<T>[] nodosInternos;
         private ArbolB<T>.Comparador<T> comparador;
 
         public Node(int n, ArbolB<T>.Comparador<T> comparador)
         {
-            this.n = n;
+            nodosInternos = new NodosInternos<T>[n - 1];
             this.comparador = comparador;
         }
 
         public bool IsEmpy()
         {
-            foreach (var item in nodosInternos)
+            if (nodosInternos[0] == null)
             {
-                if (item!=null)
-                {
-                    return false; //osea que no esta vacío
-                }
+                return true;
             }
-            return true;
+            else
+            {
+                return false;
+            }
         }
         public void AddValue(T value)
         {
@@ -48,14 +47,26 @@ namespace ArbolB
                     Value = value
                 };
             }
+
+            CorrectOrder();
         }
         public void CorrectOrder()
         {
-            for (int i = 0; i < nodosInternos.Length -1 ; i++)
+            int length = 0;
+            for (int x = 0; x < nodosInternos.Length; x++)
             {
-                for (int j = i + 1; j < nodosInternos.Length; j++)
+                if (nodosInternos[length] != null)
                 {
-                    if(comparador.Invoke(nodosInternos[i].Value,nodosInternos[j].Value) > 0)
+                    length++;
+                }
+
+
+            }
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (comparador.Invoke(nodosInternos[i].Value, nodosInternos[j].Value) > 0)
                     {
                         var t = nodosInternos[i].Value;
                         nodosInternos[i].Value = nodosInternos[j].Value;
@@ -70,12 +81,16 @@ namespace ArbolB
             {
                 if (item == null)
                 {
-                    return false; 
+                    return false;
                 }
             }
             return true;
         }
-      
+        public bool tienehijos()
+        {
+            return nodosInternos[0].Left != null;
+        }
+
     }
 
 
--- Lab01_1203819_2530019/Program.cs
+++ Lab01_1203819_2530019/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using ArbolB;
 
+
 namespace Lab01_1203819_2530019
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ingrese cantidad de valores");
+            int x = Convert.ToInt32(Console.Read());
+            ArbolB<int> Arbolprueba = new ArbolB<int>(5, Compararint);
+
+            Random numerorandom = new Random();
+            while (x != 0)
+            {
+                x--;
+                int num = numerorandom.Next(1, 200);
+                Arbolprueba.Add(num);
+
+            }
+        }
+        public static int Compararint(int x, int y)
+        {
+            int r = x.CompareTo(y);
+            return r;
         }
     }
 }
